06d153608476a80656d5c487957bc9793389444c
Saurabh Gupta <saurabhagrahari888@outlook.com>
2025-03-29 12:28:59 +0530

    Added Code For Register Pages

--- ES_TUTOR/Controller/RegisterationController.cs
+++ ES_TUTOR/Controller/RegisterationController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ES_TUTOR.Models;
+using ES_TUTOR.ViewModel.Student;
+using ES_TUTOR.ViewModel.Teacher;
+using ES_TUTOR.ViewModel.Tutor;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ES_TUTOR.Controller
 {
     public class RegisterationController : Microsoft.AspNetCore.Mvc.Controller
     {
-        public IActionResult Registration()
+        [HttpGet("StudentRegister")]
+        public IActionResult StudentRegistration()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult StudentRegistration(StudentRequest obj)
+        {
+            return View();
+        }
+
+       [Route("TeacherRegister")]
+        [HttpGet]
+        public IActionResult TeacherRegistration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TeacherRegistration(TeacherRequest obj)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TutorRegistration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TutorRegistration(TutorRequest obj)
+        {
+            return View();
+        }
+
+
+
     }
 }
---
+++ ES_TUTOR/ViewModel/Student/StudentRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace ES_TUTOR.ViewModel.Student
+{
+    public class StudentRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string? Address { get; set; }
+        public string? State { get; set; }
+        public string? City { get; set; }
+
+        public DateTime? DOB { get; set; }
+        public string Mobile { get; set; }
+        public string CoreSubject { get; set; }
+        public string StudentMedium { get; set; }//Student
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public string? IsRole { get; set; }
+    }
+}
